959195d1cbd75cbc3ad2aa44bf4c8c0b79afaa10
Andrew Polkinghorn <ampolkinghorn@gmail.com>
2018-07-14 15:07:07 +1000

    Adds initial project.

---
+++ RunTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RunTime
+//
+//  Created by Andrew Polkinghorn on 14/07/2018.
+//  Copyright © 2018 Andrew Polkinghorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
